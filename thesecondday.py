01776c05f95361f775154edb86ad0cbac5fdff66
kashish0903 <tomshreepali@gmail.com>
2026-01-23 13:50:01 +0545

    add more condition

--- thesecondday.py
+++ thesecondday.py
@@ -108,3 +108,37 @@ print(age >= 25)
 # Test 10: less than or equal to (False)
 print("\n Is age <= 20? I predict false")
 print(age <= 20)
+# More condition
+# Test for equality and inequality with strings.
+name = "Alice"
+print("string equality: name == 'Alice'..", name == 'Alice')
+print("string inequality: name != 'Bob'..", name != 'Bob')
+# TESt using the lower () method
+city = "New York"
+print("/n Using lower(): city.lower() == 'new york',,", city.lower() == 'new york')
+print("/n Case sensitive: city == 'new york',,", city == 'new york')
+# Number tests
+num1 = 15
+num2 = 20
+print("\n Equality : num1 == 15?", num1 == 15)
+print("\n INequality : num1 != 20?", num2 != 20)
+print("\n Greater than : num2 > 15?", num2 > 15)
+print("\n Less  than : num1 < 20?", num1 < 20)
+print("\n Greater than or equal to: num2 >= 15?", num2 >= 15)
+print("\n Less  than or equal to: num1 <= 20?", num1 <= 20)
+# TEST using 'and' keyword
+age = 25
+income = 5000
+print("\n Using 'and':age > 18 and income> 40000?", age > 18 and income > 40000)
+print("\n Using 'and':age > 30 and income> 40000?", age > 30 and income > 40000)
+
+# TEST using 'OR' keyword
+print("\n Using 'or':age > 30 or income> 40000?", age > 30 or income > 40000)
+print("\n Using 'or':age > 30 or income> 40000?", age > 30 or income > 30000)
+# Test whether an item is in a list
+fruits = ['apple', 'banana', 'orange']
+print("\n Item in list: 'apple' in fruits?", 'apple' in fruits)
+print("\n Item in list: 'grape' in fruits?", 'grape' in fruits)
+# Test whether an item is in not a list
+print("\n Item not in list: 'grape' not in fruits?", 'grape'not in fruits)
+print("\n Item not in list: 'apple' not in fruits?", 'apple'not in fruits)
