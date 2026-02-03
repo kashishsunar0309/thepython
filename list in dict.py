11af24519c079f22f181e313cc4dd62bd968a4b3
kashish0903 <tomshreepali@gmail.com>
2026-02-03 13:39:06 +0545

    add new dic in dic

--- list in dict.py
+++ list in dict.py
@@ -28,3 +28,45 @@ for name,language in fav_language.items():
     print(f'my name is {name} mine fav language are:')
     for languages in language:
         print(f"{languages}")
+        
+#dict in dict method
+user = {
+    'aeinstein':{
+        'first':'albert',
+        'last':'einstein',
+        'location':'princeton',
+    },
+    'mcurie':{
+        'first':'marie',
+        'last':'curie',
+        'location':'paris',
+    },    
+}
+for username,user_info in user.items():
+    print(f'\n Username: {username}')
+    full_name = f'{user_info["first"]}{user_info ["last"]}'
+    location = user_info['location']
+    print(f'\t Full name: {full_name.title()}')
+    print(f'\t location:{location.title()}')
+    
+print('')
+    
+ #self one  
+place= {
+    'nepal':{
+        'capital':'kathmandu',
+        'tourist':'porkhara',
+        'temple':'janaki temple ommmmm!',
+    },
+    'india':{
+        'capital':'new delhi',
+        'tourist':'kolkata',
+        'temple':'kadarnath temple ommmm!',
+    },
+}
+for country,place_info in place.items():
+    print(f'country_name:{country}')
+    place_name = f'{place_info["capital"]}, {place_info["tourist"]}'
+    temple_name = place_info['temple']
+    print(f'\tplace_name:{place_name.title()}')
+    print(f'\ttemple_name:{temple_name.title()}')
