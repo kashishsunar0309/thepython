018ccdf8f8d6ca2647a9d7fe9cf41e77196ea390
kashish0903 <tomshreepali@gmail.com>
2026-02-15 22:16:11 +0545

    add tryyourself 8-1 to 8-2 ex

---
+++ Chapter_Function_8/tryyourself_8_all_ex.py
@@ -0,0 +1,9 @@
+#8-1 ex tryyourself
+def display_message():
+    print("I am learning about functions in Python.")
+display_message()
+
+#8-2 ex tryyourself
+def fav_book(title):
+    print(f"One of my favorite books is {title}.")
+fav_book("Alice in Wonderland")
