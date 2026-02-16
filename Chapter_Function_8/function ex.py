5270083a3aac4e99a09d4a4a0becbe58f57ca658
kashish0903 <tomshreepali@gmail.com>
2026-02-16 21:15:43 +0545

    add function execise passing argument & other

--- Chapter_Function_8/function ex.py
+++ Chapter_Function_8/function ex.py
@@ -3,3 +3,19 @@
 def greet_user(username):
     print(f"Hello,{username.title()}")
 greet_user('kashish')
+"""
+#passing arguments
+    #positional argument
+"""
+def describe_pet(animal_type,pet_name):
+    print(f"\n I have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster','harry')
+describe_pet('dog','willie')
+describe_pet(pet_name ='hamster',animal_type ='harry') # keyword arguments
+
+#default values
+def describe_pet_1(animal_type,pet_name='willie'):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet_1(animal_type = 'cat')
