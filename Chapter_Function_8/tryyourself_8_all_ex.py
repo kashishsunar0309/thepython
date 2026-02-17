3422fe30e461e5b2e58a484f252739d8d88a36c0
kashish0903 <tomshreepali@gmail.com>
2026-02-17 21:13:19 +0545

    add 8-3 to 8-5 execise

--- Chapter_Function_8/tryyourself_8_all_ex.py
+++ Chapter_Function_8/tryyourself_8_all_ex.py
@@ -7,3 +7,21 @@ display_message()
 def fav_book(title):
     print(f"One of my favorite books is {title}.")
 fav_book("Alice in Wonderland")
+#8-3 ex tryyourself
+def make_shirt(size,message):
+    print(f"the t-shirt size is {size} and color is {message}")
+make_shirt("large","hello world!")
+    
+#8-4
+def make_shirt1(size="large",message="I love python"):
+    print(f"the sixe is {size} and {message}")
+make_shirt1()
+make_shirt1(size="medium")
+make_shirt1(size="small", message="Go Python!")
+#8-5
+def describe_city(city,country="USA"):
+    print(f"{city} is in {country}.")
+describe_city(city="Texas")
+describe_city(city="new york")
+describe_city(city="Rejkjavik",country="Iceland")
+    
