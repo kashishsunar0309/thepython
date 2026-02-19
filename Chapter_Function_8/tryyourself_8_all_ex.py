43ae5a723a685724da419cc5bec7e799dc6039f2
kashish0903 <tomshreepali@gmail.com>
2026-02-19 18:30:11 +0545

    add ex 8-6 to 8-8

--- Chapter_Function_8/tryyourself_8_all_ex.py
+++ Chapter_Function_8/tryyourself_8_all_ex.py
@@ -24,4 +24,48 @@ def describe_city(city,country="USA"):
 describe_city(city="Texas")
 describe_city(city="new york")
 describe_city(city="Rejkjavik",country="Iceland")
-    
+# 8-6: City and Country
+def city_country(city, country):
+    return f"{city.title()}, {country.title()}"
+
+print(city_country("mumbai", "india"))
+print(city_country("hongkong", "china"))
+print(city_country("santiago", "chile"))
+
+
+# 8-7: Make Album
+def make_album(artist_name, album_title, songs=None):
+    album = {
+        'artist': artist_name,
+        'title': album_title
+    }
+    if songs:
+        album['songs'] = songs
+    return album
+
+print(make_album('Kashish', 'Desi Girl'))
+print(make_album('Ram', 'All Girls Are Same'))
+print(make_album('Arijit', 'Aashiqui', songs=12))
+
+
+# 8-8: User Albums
+def make_album1(artist_name, album_title, songs=None):
+    album = {
+        'artist': artist_name,
+        'title': album_title
+    }
+    if songs:
+        album['songs'] = songs
+    return album
+
+while True:
+    print("\n--- Album Entry ---")
+    artist = input("Enter artist name (or 'q' to quit): ")
+    if artist == 'q':
+        break
+
+    title = input("Enter album title (or 'q' to quit): ")
+    if title == 'q':
+        break
+
+    print(make_album(artist, title))
