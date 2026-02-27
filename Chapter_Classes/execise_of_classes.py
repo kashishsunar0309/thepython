aa0ad97b462c1913044afa6a9384484423c8afe6
kashish0903 <tomshreepali@gmail.com>
2026-02-27 22:21:40 +0545

    add multiple instance

--- Chapter_Classes/execise_of_classes.py
+++ Chapter_Classes/execise_of_classes.py
@@ -7,5 +7,10 @@ class Dog:
     def rool_over(self):
         print(f"{self.name} roolled over! ")
 my_dog = Dog('Willie',6)
+your_dog = Dog('lucy',3)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
+#multiple Instance
+print(f"\n Your dog's name is {your_dog.name}")
+print(f"Your dog is {your_dog.age}years old.")
+your_dog.sit
