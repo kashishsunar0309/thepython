f2af565d1278ed445a4ae799105bb4653d52cd85
kashish0903 <tomshreepali@gmail.com>
2026-02-28 22:13:00 +0545

    add try-yourself-9-1 execise

---
+++ Chapter_Classes/try_yourself_9_all_ex.py
@@ -0,0 +1,20 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"Cuisine Type: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
+
+restaurant = Restaurant("The Golden Fork", "Italian")
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+print("\n" + "="*40 + "\n")
+#9-2
