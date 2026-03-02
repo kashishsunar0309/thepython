4cb3cc62b2a0c0302d2bd6e79bd0bc980e1cc9ea
kashish0903 <tomshreepali@gmail.com>
2026-03-02 21:20:27 +0545

    add setting a default valie of a attribute

--- Chapter_Classes/execise_of_classes.py
+++ Chapter_Classes/execise_of_classes.py
@@ -28,3 +28,17 @@ class Car:
 
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
+#setting a default value of a attribute
+class car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        return f"{self.make}{self.model}{self.year}"
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading}miles on it.")
+my_new_car1 = car('bmw','m4','2022')
+print(my_new_car1.get_descriptive_name())
+my_new_car1.read_odometer()
