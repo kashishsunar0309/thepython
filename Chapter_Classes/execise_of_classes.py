9444bad4f2ae97dc542dd41c08aa352d39bea421
kashish0903 <tomshreepali@gmail.com>
2026-03-04 16:18:04 +0545

    Add some increament and modifying attri

--- Chapter_Classes/execise_of_classes.py
+++ Chapter_Classes/execise_of_classes.py
@@ -21,35 +21,32 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-
-my_new_car = Car('audi', 'a4', 2024)
-print(my_new_car.get_descriptive_name())
-#setting a default value of a attribute
-class Vehicle:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
         self.odometer_reading = 0
 
     def get_descriptive_name(self):
         return f"{self.year} {self.make} {self.model}"
 
     def read_odometer(self):
-        print(f"This vehicle has {self.odometer_reading} miles on it.")
+        print(f"This car has {self.odometer_reading} miles on it.")
 
     def update_odometer(self, mileage):
-        self.odometer_reading = mileage
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
 
 
-my_new_vehicle = Vehicle('porsche', 'A4', 2020)
+my_new_car = Car('porsche', 'A4', 2020)
+print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
 
-print(my_new_vehicle.get_descriptive_name())
-
-my_new_vehicle.update_odometer(23)
-
-my_new_vehicle.read_odometer()
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
