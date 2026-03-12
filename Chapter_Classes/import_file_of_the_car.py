661586e632ab82b863a1f2afb38e769466a9c406
kashish0903 <tomshreepali@gmail.com>
2026-03-12 22:12:08 +0545

    add multiple class from a module

--- Chapter_Classes/import_file_of_the_car.py
+++ Chapter_Classes/import_file_of_the_car.py
@@ -3,3 +3,9 @@ my_new_car = Car('audi','a4',2024)
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()'''
+# from here below that import multiple class from a module
+from the_car import Car, ElectricCar
+my_mustang = Car('FORD',"mustang",2005)
+print(my_mustang.get_descriptive_name())
+my_leaf = ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
---
+++ Chapter_Classes/the_car.py
@@ -0,0 +1,44 @@
+# The Car class
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        return f"{self.year} {self.make} {self.model}"
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+# Instance as Attribute
+class Battery:
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kwh battery.")
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+        print(f"This car can go about {range} miles on a full charge.")
+        
+        
+# Inheritance
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()  # composition
