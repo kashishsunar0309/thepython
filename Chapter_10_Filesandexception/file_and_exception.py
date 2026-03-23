04af9a784acfb25469ad425ff8d306a39e949233
kashish0903 <tomshreepali@gmail.com>
2026-03-23 20:44:55 +0545

    add single file and multi

--- Chapter_10_Filesandexception/file_and_exception.py
+++ Chapter_10_Filesandexception/file_and_exception.py
@@ -10,3 +10,14 @@ if birthday in pi_string:
     print("Your birth appears in the first million digits of pi ! ")
 else:
     print('Your birthday doesnot appear in the first million digits of pi.')
+#second execise of file and exception.
+"""from pathlib import Path
+contents = "I love programming.\n"
+contents += 'I love creating new games.\n'
+contents += 'I also love working with data.\n'
+path = Path('programing.txt')
+#writing single file
+#path.write_text("I love Math and progamming with connection between there.")
+#writing multi line file
+path.write_text(contents)
+"""
