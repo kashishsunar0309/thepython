bd3b8c8d368e6a6cddcf117eda5d504a6ec11bc1
kashish0903 <tomshreepali@gmail.com>
2026-03-24 20:38:57 +0545

    add 10-4 & 10-5 exc

--- Chapter_10_Filesandexception/try_yourself_10.py
+++ Chapter_10_Filesandexception/try_yourself_10.py
@@ -20,3 +20,24 @@ path = Path('learning_python.txt')
 contents = path.read_text()
 for line in contents.splitlines():
     print(line.replace('Python','C'))
+    """"
+#10-4
+from pathlib import Path
+name = input("What's is your name: ")
+path = Path('guest.txt')#To saw the world you should create file name
+#guest.txt then what give name in terminal that automatic print in guest.txt file
+path.write_text(name)"""
+"""
+#10-5
+from pathlib import Path
+print("Type 'quit', to stop.\n")
+contents = ""
+while True:
+    name = input('enter the name:')
+    if name.lower() == "quit":
+        break
+    contents += name + '\n'
+path = Path("guest_boook.txt")#where program work to see create file name
+#guest_book.txt where what enter the terminal that will print in guest_book.txt file
+path.write_text(contents)   
+"""
