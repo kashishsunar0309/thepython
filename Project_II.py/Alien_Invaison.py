6ea91ac6d74d9f067d4cb3e6e11a557e4d008fa2
kashish0903 <tomshreepali@gmail.com>
2026-04-10 20:53:09 +0545

    add bg color and clock tick and import system display

--- Project_II.py/Alien_Invaison.py
+++ Project_II.py/Alien_Invaison.py
@@ -1,16 +1,51 @@
-import sys
+"""import sys
 import pygame
 class AlienInvasion:
+    #Over all class to manage game assets and behavior.
     def __init__(self):
+        #Initialize the game, and creates game resources.
         pygame.init()
-        self.screen = pygame.display.set_mode((1400,700))
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1400,800))
         pygame.display.set_caption("AlienInvasion")
+        #Set the background color.
+        self.bg_color = (255,0,0)
     def run_game(self):
+        #Start the main loop for game.
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    #Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
 if __name__ == "__main__":
+    #Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+"""
+import sys
+import pygame
+from settings import Settings
+class AlienInvasion:
+    def __init__(self):
+        #Initialize the game, and create game resources
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+    def run_game(self):
+         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    self.screen.fill(self.settings.bg_color)
+                    pygame.display.flip()
+                    self.clock.tick(60)
+if __name__ == "__main__":
+    #Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
