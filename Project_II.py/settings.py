03b852272e696eaebf943c7158c2158e88a525b7
kashish0903 <tomshreepali@gmail.com>
2026-04-12 20:51:35 +0545

    add update_screen and check_events

--- Project_II.py/settings.py
+++ Project_II.py/settings.py
@@ -3,6 +3,6 @@ class Settings:
     def __init__(self):
         #Initialize the game's settings.
         #Screen settings
-        self.screen_width = 1300
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255,255,255)
