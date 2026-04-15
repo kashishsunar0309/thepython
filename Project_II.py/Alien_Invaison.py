eddf011a6f1f78926a4cfdf809d8b630bb254d16
kashish0903 <tomshreepali@gmail.com>
2026-04-15 23:17:32 +0545

    add left arrow movement in python

--- Project_II.py/Alien_Invaison.py
+++ Project_II.py/Alien_Invaison.py
@@ -31,7 +31,6 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
 class AlienInvasion:
     def __init__(self):
         # Initialize the game, and create game resources
@@ -43,20 +42,18 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
-
     def run_game(self):
         while True:
             # Watch for keyboard and mouse events
             self._check_events()
             self._update_screen()
+            self.ship.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
             # Redraw the screen each pass through the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-
             # Make the most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
@@ -67,14 +64,19 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        #Move the ship to the right.
-                        self.ship.rect.x +=1
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
     def _update_screen(self):
         "Update images on the screen, and flip to the new screen."
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        pygame.display.flip()
-                
+        pygame.display.flip()                
 if __name__ == "__main__":
     # Make a game instance, and run the game
     ai = AlienInvasion()
--- Project_II.py/ship.py
+++ Project_II.py/ship.py
@@ -23,10 +23,13 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
         #Movement flag; start with a ship that's not moving.
         self.moving_right = False
+        self.moving_left = False
     def update(self):
         #Updates the ship's position based on the movement flag.
         if self.moving_right:
             self.rect.x +=1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location"""
