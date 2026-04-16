3c4f4c8a8210549e41839bb38f5a9e09595777ce
kashish0903 <tomshreepali@gmail.com>
2026-04-16 20:50:50 +0545

    add ship speed and corner limit

--- Project_II.py/ship.py
+++ Project_II.py/ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect
         self.image = pygame.image.load(os.path.join(os.path.dirname(__file__), "image", "alien.png"))
@@ -21,15 +22,22 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        #store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
         #Movement flag; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
     def update(self):
         #Updates the ship's position based on the movement flag.
-        if self.moving_right:
-            self.rect.x +=1
-        if self.moving_left:
-            self.rect.x -= 1
+        #Updates the ship's value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+            #self.rect.x +=1 // that the concept of right move in pygame
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            #self.rect.x -= 1 // that the concept of left move in pygame
+        #Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
