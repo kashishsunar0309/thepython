38b83f030fac40c524d94836367f80f3c8fc337f
kashish0903 <tomshreepali@gmail.com>
2026-04-26 21:08:02 +0545

    add play click system and mouse visible arrow

--- Project_II.py/Alien_Invaison.py
+++ Project_II.py/Alien_Invaison.py
@@ -46,7 +46,23 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    def _check_play_button(self,mouse_pos):
+        #Start a new game when the player clicks Play.
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            #Reset the game statistics.
+            self.stats.reset_stats()
+            self.game_active = True
+            #Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+            #Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+          
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
@@ -126,6 +142,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
     def _check_aliens_bottom(self):
         #Check if any aliens have reached and the bottom of the screen.
         for alien in self.aliens.sprites():
