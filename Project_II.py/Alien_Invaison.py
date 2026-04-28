0638159faeec56ec66dec1b6be3fe594acfecd05
kashish0903 <tomshreepali@gmail.com>
2026-04-28 22:18:29 +0545

    add error and problem

--- Project_II.py/Alien_Invaison.py
+++ Project_II.py/Alien_Invaison.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from time import sleep
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -19,6 +20,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         #Create an instance to store game statistics
         self.stats = GameStats(self)
+        #Create an instance to store game stastistics,
+        #and create a scoreboard.
+        self.sb = Scoreboard(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -167,6 +171,8 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+        #Draw the score information.
+        self.sb.show_score()
         #Draw the play button if the game is inactive.
         if not self.game_active:
             self.play_button.draw_button()
