b0a7655090877563530d99c252033a2dd7994e72
kashish0903 <tomshreepali@gmail.com>
2026-05-04 20:43:00 +0545

    add head and x and y

--- Generating_Data.py/mql_square.py
+++ Generating_Data.py/mql_square.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
-squares = [1,4,9,16,25,36,49,64]
+squares = [1,4,9,16,25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares,linewidth = 2)
+#Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize = 15)
+ax.set_xlabel("Value",fontsize = 15)
+ax.set_ylabel("Square of Value", fontsize = 15)
+#Set size of trick labels.
+ax.tick_params(labelsize = 14)
 plt.show()
