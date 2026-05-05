8316ad5a2397fbb4ace2f079eaef293251038d45
kashish0903 <tomshreepali@gmail.com>
2026-05-05 20:43:02 +0545

    add point in y= 4 and x=2 center

--- Generating_Data.py/mql_square.py
+++ Generating_Data.py/mql_square.py
@@ -6,12 +6,15 @@ plt.style.use('seaborn-v0_8')
 #plt.style.use('classic')
 #plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(input_values,squares,linewidth = 3)
-ax.plot(squares,linewidth = 2)
+ax.scatter(2,4,s = 200)
+#Set chart title and label axes.
+#ax.plot(input_values,squares,linewidth = 3)
+#ax.plot(squares,linewidth = 2)
 #Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize = 15)
 ax.set_xlabel("Value",fontsize = 15)
 ax.set_ylabel("Square of Value", fontsize = 15)
 #Set size of trick labels.
 ax.tick_params(labelsize = 14)
+
 plt.show()
