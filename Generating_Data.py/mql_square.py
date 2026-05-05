690faf2855a886e2bf07abc98baf42c68f4add25
kashish0903 <tomshreepali@gmail.com>
2026-05-05 20:45:29 +0545

    add x and y value for multiple line

--- Generating_Data.py/mql_square.py
+++ Generating_Data.py/mql_square.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
-input_values = [1,2,3,4,5]
-squares = [1,4,9,16,25]
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
 plt.style.use('seaborn-v0_8')
 #plt.style.use('ggplot')
 #plt.style.use('classic')
 #plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(2,4,s = 200)
+ax.scatter(x_values,y_values, s = 100)
 #Set chart title and label axes.
 #ax.plot(input_values,squares,linewidth = 3)
 #ax.plot(squares,linewidth = 2)
