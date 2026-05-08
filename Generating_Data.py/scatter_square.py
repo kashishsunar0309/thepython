2814f23480ac06f1396671f95be0a8535104dd5e
kashish0903 <tomshreepali@gmail.com>
2026-05-08 17:20:39 +0545

    fix error

--- Generating_Data.py/scatter_square.py
+++ Generating_Data.py/scatter_square.py
@@ -3,12 +3,13 @@ x_values = range(1,1001)
 y_values = [x**2 for x in x_values]
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
-#ax.scatter(x_values,y_values,c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_values,y_values,c=y_values, cmap='Blues', s=10)
 #Set the range for each axis.
 ax.set_title("Square Numbers", fontsize = 15)
 ax.set_xlabel("Value",fontsize = 15)
 ax.set_ylabel("Square of Value", fontsize = 15)
 #Set size of trick labels.
 ax.tick_params(labelsize = 14)
-'''ax.axis([0, 1100, 0, 1_100_000])'''# [xmin, xmax, ymin, ymax]
+ax.axis((0.0, 1100.0, 0.0, 1_100_000.0))# (xmin, xmax, ymin, ymax)
+plt.savefig('squares_plot.png',bbox_inches = 'tight')
 plt.show()
