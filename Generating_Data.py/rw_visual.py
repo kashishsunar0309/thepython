edbfd61e7cc656bc26d2b8963cfe5249523138e9
kashish0903 <tomshreepali@gmail.com>
2026-05-10 20:32:56 +0545

    add asking quit option

--- Generating_Data.py/rw_visual.py
+++ Generating_Data.py/rw_visual.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
-
-rw = RandomWalk()
-rw.fill_walk()
-fig, ax = plt.subplots()
-plt.style.use('classic')
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+#Keeping making new walks,as long as the program is active.
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    fig, ax = plt.subplots()
+    plt.style.use('classic')
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap='Blues',
+               edgecolors = 'none',s=15)
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+    keep_running = input("Make another Walk? (y/n): ")
+    if keep_running == 'n':
+        break
