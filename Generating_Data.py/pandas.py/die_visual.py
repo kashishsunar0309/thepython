7070e40a75db7d819be30fe712bf826817f94e6a
kashish0903 <tomshreepali@gmail.com>
2026-05-14 20:21:39 +0545

    add die result from pandas

---
+++ Generating_Data.py/pandas.py/die_visual.py
@@ -0,0 +1,9 @@
+from die import Die
+#create a D6.
+die = Die()
+#Make some rolls, and store results in a list.
+results = []
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+print(results)
