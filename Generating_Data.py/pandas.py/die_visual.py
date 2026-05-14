cd6e41a9ec3bd76a79f17f2b21e4763960cac7f2
kashish0903 <tomshreepali@gmail.com>
2026-05-14 20:47:07 +0545

    add chart

--- Generating_Data.py/pandas.py/die_visual.py
+++ Generating_Data.py/pandas.py/die_visual.py
@@ -1,9 +1,22 @@
 from die import Die
-#create a D6.
+import plotly.express as px
+
+# Create a D6.
 die = Die()
-#Make some rolls, and store results in a list.
+
+# Make some rolls, and store results in a list.
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
-print(results)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
