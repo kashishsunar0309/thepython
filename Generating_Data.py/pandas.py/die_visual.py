d93bfbd82736d5fe2d69be1205d25ef24d4bf561
kashish0903 <tomshreepali@gmail.com>
2026-05-15 20:35:53 +0545

    add Two die mean direction char

--- Generating_Data.py/pandas.py/die_visual.py
+++ Generating_Data.py/pandas.py/die_visual.py
@@ -1,24 +1,27 @@
 from die import Die
 import plotly.express as px
 
-# Create a D6.
-die = Die()
+# Create Two a D6 dice.
+die_1 = Die()
+die_2 = Die()
+
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll()+die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-poss_results = range(1, die.num_sides + 1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-title = "Result of Rolling One D6 1,000 Times"
+title = "Result of Rolling TWO D6 1,000 Times"
 labels = {"x":'Result','y':'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies,title=title,labels=labels)
 fig.show()
