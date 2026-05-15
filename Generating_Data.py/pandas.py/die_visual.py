da9a6df16a7e6f5582d9868c496ff591202ef0fd
kashish0903 <tomshreepali@gmail.com>
2026-05-15 20:29:47 +0545

    add Head and define x and y

--- Generating_Data.py/pandas.py/die_visual.py
+++ Generating_Data.py/pandas.py/die_visual.py
@@ -18,5 +18,7 @@ for value in poss_results:
     frequencies.append(frequency)
 
 # Visualize the results.
-fig = px.bar(x=poss_results, y=frequencies)
+title = "Result of Rolling One D6 1,000 Times"
+labels = {"x":'Result','y':'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies,title=title,labels=labels)
 fig.show()
