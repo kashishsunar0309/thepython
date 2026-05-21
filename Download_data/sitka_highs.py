33d475f84a7634d6203153986b27e88412cbfc6f
kashish0903 <tomshreepali@gmail.com>
2026-05-21 13:23:18 +0545

    check push code

--- Download_data/sitka_highs.py
+++ Download_data/sitka_highs.py
@@ -4,4 +4,6 @@ path = Path('detail.csv')
 lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
 print(header_row)
