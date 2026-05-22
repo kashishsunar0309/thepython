f9e8fa690b6afa2f685b42ed9ab72ef73fbbd356
kashish0903 <tomshreepali@gmail.com>
2026-05-22 13:22:21 +0545

    add graph style download data

--- Download_data/sitka_highs.py
+++ Download_data/sitka_highs.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
 
 # Step 1: Open the CSV file
-path = Path('detail.csv')
+path = Path(__file__).parent / 'detail.csv'
 lines = path.read_text().splitlines()
 
 # Step 2: Read the CSV content
@@ -11,9 +12,22 @@ reader = csv.reader(lines)
 # Step 3: Get the first row (header row)
 header_row = next(reader)
 
-#Exatracting high tempature
+# Extracting high temperature
 highs = []
 for row in reader:
     high = int(row[4])
     highs.append(high)
+
+# Plot the high temperatures
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot
+ax.set_title("Daily High Temperature, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
 print(highs)
+plt.show()
