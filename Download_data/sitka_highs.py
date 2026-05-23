869384bb758da37a189cbc02fda03f6bf8d11ccc
kashish0903 <tomshreepali@gmail.com>
2026-05-23 11:52:44 +0545

    second data series

--- Download_data/sitka_highs.py
+++ Download_data/sitka_highs.py
@@ -14,21 +14,24 @@ reader = csv.reader(lines)
 header_row = next(reader)
 
 # Extracting high temperature
-dates, highs = [], []
+dates, highs , lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(highs,color = 'red')
 ax.plot(highs, color='red')
+ax.plot(lows, color = 'blue')
 
 # Format plot
-ax.set_title("Daily High Temperature, 2021", fontsize=24)
+ax.set_title("Daily High Temperature and Low Temperature, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
