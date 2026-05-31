a3ca7c818feb826e2b59129ba904db8370153593
kashish0903 <tomshreepali@gmail.com>
2026-05-31 10:06:55 +0545

    set up complete

--- Download_data/eq_explore_data.py
+++ Download_data/eq_explore_data.py
@@ -12,10 +12,14 @@ path.write_text(readable_contents)
 
 #Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
-
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
