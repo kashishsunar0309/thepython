3746f8e40efa2140b46efec8c30e9ac0dbefb6ae
kashish0903 <tomshreepali@gmail.com>
2026-06-03 09:50:41 +0545

    add visualizing repositories using plotly

--- API_CODE/python_repos.py
+++ API_CODE/python_repos.py
@@ -1,12 +1,13 @@
 import requests
-
+import plotly.express as px
+#Make an API call and check the response
 url = "https://api.github.com/search/repositories"
 url += "?q=language:python+sort:stars+stars:>10000"
 headers = {"Accept": "application/vnd.github.v3+json"}
 
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
-
+#Process overall results
 response_dict = r.json()
 print(response_dict.keys())
 
@@ -16,16 +17,12 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 #Explore information about the repositories.
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
-
+repo_names, stars = [], []
 #Examine the first repository.
-repo_dict = repo_dicts[0]
-print("\n Selected information about each repository: ")
 for repo_dict in repo_dicts:
-    print(f"\n Keys: {len(repo_dict)}")
-    print("\n Selected information about first repository:")
-    print(f"\n Name: {repo_dict['name']}")
-    print(f"Owner:{repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Updated: {repo_dict['created_at']}")
-    print(f"Description: {repo_dict['description']}")
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    
+#Make visualization.
+fig = px.bar(x=repo_names, y= stars)
+fig.show()
