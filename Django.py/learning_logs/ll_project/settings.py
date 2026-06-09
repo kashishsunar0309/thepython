c74cbbc3aa2cee4a95c170b9506b917fd224534f
kashish0903 <tomshreepali@gmail.com>
2026-06-09 09:36:19 +0545

    add django basic program but error

---
+++ Django.py/learning_logs/ll_project/settings.py
@@ -0,0 +1,9 @@
+INSTALLED_APPS = [
+    'learning_logs',  # add this line
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
