7aaeff36313db1004b904de4b82473109ef47001
kashish0903 <tomshreepali@gmail.com>
2026-09-01 19:19:06 +0545

    add concept of private

--- encapsulation/private_example.py
+++ encapsulation/private_example.py
@@ -1,7 +1,7 @@
 class person:
     def __init__(self, name, age):
         self.name = name
-        self._age = age  # private property
+        self.__age = age  # private property
 p1 = person("Emil", 26)
 print(p1.name)
-print(p1._age)  # This will cause an error.
+print(p1.__age)  # This will cause an error.
